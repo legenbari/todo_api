4a81adcd73b458b1769df72e790f893ec19b6736
Artem <legenbari@mail.ru>
2026-04-03 11:30:19 +0300

    Есть get по id и get_all

--- crud.py
+++ crud.py
@@ -1,7 +1,10 @@
 from datetime import date
 
+from fastapi import HTTPException
+
 from models import NotesModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 
 
@@ -15,4 +18,11 @@ async def post(post_scheme, session: AsyncSession):
     await session.commit()
     await session.refresh(new_note)
     return new_note
-    
+
+async def get_all(session: AsyncSession):
+    result = await session.execute(select(NotesModel))
+    return result.scalars().all()
+
+async def get(id, session: AsyncSession):
+    result = await session.execute(select(NotesModel).where(NotesModel.id==id))
+    return result.scalars().all()
--- main_api.py
+++ main_api.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from schemes import PostScheme
 from models import get_session
-from crud import post
+from crud import post, get_all, get
 from fastapi import Depends
 
 app = FastAPI()
 
 
-@app.post("/note/post")
+@app.post("/note/post", summary="Новая заметка")
 async def note_post(post_scheme: PostScheme, session: AsyncSession = Depends(get_session)):
     note = await post(post_scheme=post_scheme, session=session)
     return {"message":"Всё в порядке", "note":note.title}
+
+@app.get("/note/get", summary="Все заметки")
+async def note_get(session: AsyncSession = Depends(get_session)):
+    notes = await get_all(session=session)
+    if not notes:
+        raise HTTPException(status_code=404, detail="Ошибка")
+    return {"message":"Всё в порядке", "notes":notes}
+
+
+@app.get("/note/get/{note_id}", summary="Заметка с конкретным id")
+async def note_get(note_id: int, session: AsyncSession = Depends(get_session)):
+    note = await get(id=note_id, session=session)
+    if not note:
+        raise HTTPException(status_code=404, detail=f"Записи с id {note_id} нет")
+    return {"message":"Всё в порядке", "notes":note}
